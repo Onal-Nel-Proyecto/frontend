5e989e4a5c2ec648ec9eb7a214517168f62dddb9
delioomega <deliorafaelcantillovilla@gmail.com>
2026-05-05 14:21:24 -0500

    perro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-import Pagelogin from './page/login2/login'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
 
 function App() {
   return (
