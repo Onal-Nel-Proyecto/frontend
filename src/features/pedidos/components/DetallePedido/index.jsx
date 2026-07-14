0395d93bfa0fe95f59e62f6b07992f6b07eb496e
WilDev <wilfridorafaellawson@gmail.com>
2026-07-14 15:36:10 -0500

    modificacion de url de imagenes de referencia

--- src/features/pedidos/components/DetallePedido/index.jsx
+++ src/features/pedidos/components/DetallePedido/index.jsx
@@ -33,8 +33,8 @@ const DetallePedido = () => {
       setImages(pedido.fotos_pedido.map((f) => ({
         id: f.foto_id,
         foto_id: f.foto_id,
-        preview: `${API_URL}${f.foto_url}`,
-        // preview: `${f.foto_url}`,
+        // preview: `${API_URL}${f.foto_url}`,
+        preview: `${f.foto_url}`,
         name: f.foto_url.split('/').pop() || `foto-${f.foto_id}`,
         isExisting: true,
       })));
